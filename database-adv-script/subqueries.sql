cc03dd67da5d9fd7dee37c682118024209a4847c
Mackachila <117160475+Mackachila@users.noreply.github.com>
2025-05-18 13:41:43 +0300

    Create subqueries.sql

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,26 @@
+ Non-Correlated Subquery
+SELECT 
+    property_id,
+    name,
+    location,
+    pricepernight
+FROM Property
+WHERE property_id IN (
+    SELECT property_id
+    FROM Review
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
+
+Correlated Subquery
+SELECT 
+    user_id,
+    first_name,
+    last_name,
+    email
+FROM User u
+WHERE (
+    SELECT COUNT(*) 
+    FROM Booking b 
+    WHERE b.user_id = u.user_id
+) > 3;
