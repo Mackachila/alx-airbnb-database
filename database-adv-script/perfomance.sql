17f36cb0351f53e3ced3bbd1d6eae4cf3d43842a
Mackachila <117160475+Mackachila@users.noreply.github.com>
2025-05-18 14:11:17 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -58,6 +58,8 @@ SELECT
 FROM Booking b
 JOIN User u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed'
+  AND b.start_date >= '2025-06-01';
 
 EXPLAIN ANALYZE SELECT ...
